f453deb71260909bc61e075ab36bfd62acfec847
Sulfur-CK <sulfur@chainkemists.com>
2025-01-27 07:22:03 -0800

    feat: added bat files to launch packaged builds with logs/traces

---
+++ CkRun_LogOnly.bat
@@ -0,0 +1,16 @@
+@echo off
+:: Find the first .exe file in the folder
+for %%f in (*.exe) do (
+    set EXE_NAME=%%~nf
+    goto RunGame
+)
+
+:RunGame
+if not defined EXE_NAME (
+    echo No executable found in the current directory.
+    pause
+    exit /b
+)
+
+:: Launch the game with the given parameters
+%EXE_NAME%.exe -log
